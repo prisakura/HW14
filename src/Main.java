576d42472e7720412469667cc3ce493efc439538
Artem Martynov <amerta90@mail.ru>
2023-03-03 20:43:41 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int input = new Scanner(System.in).nextInt();
+        System.out.println(avg(input));
+    }
+
+    public static double avg(int num) {
+        //todo Дописать логику работы метода сюда. Метод main не трогаем!!!
+        return 0; //todo заменить 0 на корректный результат.
+    }
+}
